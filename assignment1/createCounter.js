e58136e41f37cb99477a44705ce0e2bce53e133f
yagyagoel1 <yagyagoel87@gmail.com>
2023-12-29 01:24:24 +0530

    created a folder for assignment 1

---
+++ assignment1/createCounter.js
@@ -0,0 +1,9 @@
+//create a counter using java script that counts down from 30 to 0
+function count(endingCount)
+{
+    for(let i=endingCount;i>=0;i--)
+    {
+        console.log(i);
+    }
+}
+count(30);
